9e6fa09e842dac85740bc3b134f58f220e3c5bbd
unknown <Cammilleeee>
2024-07-05 23:24:28 +0800

    fix layout

--- src/pages/PocHead/PocHeadDashb.jsx
+++ src/pages/PocHead/PocHeadDashb.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Route, Routes, Link } from 'react-router-dom';
+import { Route, Routes, Link} from 'react-router-dom';
 import './PocHeadDashb.css';
 
 function Header() {
